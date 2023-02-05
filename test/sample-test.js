85f755a2bee526107fa89ded03be210e2fbc7eff
cyberpunkych <cyber-punk@xakep.ru>
2023-02-05 01:26:15 +0100

    1

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,10 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
+describe("EvilNFT", function () {
   it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
+    const Greeter = await ethers.getContractFactory("EvilNFT");
+    const greeter = await Greeter.deploy();
     await greeter.deployed();
-
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });
